ad54e3df584d7a0f088d8ca362c40e836882cae2
Thilina <samarawickramathilina@gmail.com>
2021-08-14 01:30:40 +1000

    Eloquent 1st commit

--- app/controllers/home.php
+++ app/controllers/home.php
@@ -2,12 +2,22 @@
 
 class Home extends Controller {
 
+    protected $user;
+
+    public function __construct(){
+        
+        $this->user = $this->model('User');
+
+    }
 
     public function index($name = ''){
 
         //echo 'home/index';
 
-       $user =  $this->model('User');//pass the Class Name User to method model in Controller class
+       //$user =  $this->model('User');//pass the Class Name User to method model in Controller class
+
+       $user =  $this->user;
+
        $user->name = $name;// User class object -> name variable
        //echo $user->name;
 
@@ -15,6 +25,29 @@ class Home extends Controller {
        $this->view('home/index', ['name'=>$user->name]);
 
        //$this->view('home/index', ['name'=>$user->name,'emails'=>'something']);
+
+    }
+
+    //just demo. we are normally not doing this way
+    public function create($username = '', $email = ''){
+
+        /*$this->user->create([
+            'username' => $username,
+            'email' => $email
+        ]);*/
+
+        User::create([
+            'username' => $username,
+            'email' => $email
+        ]);
+
+
+    }
+
+    public function update($username = '', $email =''){
+
+        User::where('username', $username)
+            ->update(['email' => $email]);
 
     }
 
--- app/models/User.php
+++ app/models/User.php
@@ -1,14 +1,40 @@
 <?php
 
-    class User{
+use Illuminate\Database\Eloquent\Model as Eloquent;
+
+    class User extends Eloquent{
 
 
 
         public $name;
 
+        //public $timestamps = [];
+
+        protected $fillable = ['username','email'];
+
+        
 
         
     }
+
+
+
+    //db table structure
+
+    /*
+
+    Database: `website`
+
+    CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `username` varchar(20) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT NULL,
+  `updated_at` timestamp NULL DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+
+    */
 
 
 
